8cd7c03cf8261efbe94efa9e848a72a3c6976d33
Younes Laaroussi <01ylaaroussi01@gmail.com>
2025-07-15 19:18:03 -0300

    remove unused packages

--- src/components/typewriter/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as MarkdownTypewriter } from "./MarkdownTypewriter";
-export { default as MarkdownTypewriterAsync } from "./MarkdownTypewriterAsync";
-export { default as MarkdownTypewriterHooks } from "./MarkdownTypewriterHooks";
--- src/components/typewriter/interfaces/index.ts
+++
@@ -1,2 +0,0 @@
-export type { MarkdownTypewriterHooksProps, default as MarkdownTypewriterProps } from "./MarkdownTypewriterProps";
-
